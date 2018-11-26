f738aaa1f6ff17bfc9eece3509d0c3791f50e3d1
Caitlin Harrington <cnharrington97@gmail.com>
2018-11-26 14:53:48 -0500

    step 1 unix script

--- align.sh
+++ align.sh
@@ -1,4 +1,6 @@
-#usage bash align.sh  
+#step 1 of project
+#combine the first lines of the BLAST gene results from the alignment csv texts downloaded from BASH
+#usage bash align.sh when in the Bioinformatics_Group_Project directory
 
 for file in ./*.csv
 	do
