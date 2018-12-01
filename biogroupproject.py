19de9e07c980038f9149ca091edf7ef68a70c453
Caitlin Harrington <cnharrington97@gmail.com>
2018-12-01 10:59:11 -0500

    definite final script for making graphs

--- biogroupproject.py
+++ biogroupproject.py
@@ -22,22 +22,29 @@ import pandas
 from plotnine import *
 
 a=ggplot(hitsframe1,aes(x='hitsframe1.iloc[:,0]',y='hitsframe1.iloc[:,1]'))+xlab("sequence1")+ylab("hits")
-a+geom_bar(stat="summary")+scale_x_discrete(labels=l)
+g=a+geom_bar(stat="summary")+scale_x_discrete(labels=l)
+ggsave(filename="sequence1hits.pdf", plot=g)
+
 
 b=ggplot(hitsframe2,aes(x='hitsframe2.iloc[:,0]',y='hitsframe2.iloc[:,1]'))+xlab("sequence2")+ylab("hits")
-b+geom_bar(stat="summary")+scale_x_discrete(labels=l)
+g=b+geom_bar(stat="summary")+scale_x_discrete(labels=l)
+ggsave(filename="sequence2hits.pdf", plot=g)
 
 c=ggplot(hitsframe6,aes(x='hitsframe6.iloc[:,0]',y='hitsframe6.iloc[:,1]'))+xlab("sequence6")+ylab("hits")
-c+geom_bar(stat="summary")+scale_x_discrete(labels=l)
+g=c+geom_bar(stat="summary")+scale_x_discrete(labels=l)
+ggsave(filename="sequence6hits.pdf", plot=g)
 
 d=ggplot(hitsframe8,aes(x='hitsframe8.iloc[:,0]',y='hitsframe8.iloc[:,1]'))+xlab("sequence8")+ylab("hits")
-d+geom_bar(stat="summary")+scale_x_discrete(labels=l)
+g=d+geom_bar(stat="summary")+scale_x_discrete(labels=l)
+ggsave(filename="sequence8hits.pdf", plot=g)
 
 e=ggplot(hitsframe9,aes(x='hitsframe9.iloc[:,0]',y='hitsframe9.iloc[:,1]'))+xlab("sequence9")+ylab("hits")
-e+geom_bar(stat="summary")+scale_x_discrete(labels=l)
+g=e+geom_bar(stat="summary")+scale_x_discrete(labels=l)
+ggsave(filename="sequence9hits.pdf", plot=g)
 
 f=ggplot(hitsframe10,aes(x='hitsframe10.iloc[:,0]',y='hitsframe10.iloc[:,1]'))+xlab("sequence10")+ylab("hits")
-f+geom_bar(stat="summary")+scale_x_discrete(labels=l)
+g=f+geom_bar(stat="summary")+scale_x_discrete(labels=l)
+ggsave(filename="sequence10hits.pdf", plot=g)
 
 
 
