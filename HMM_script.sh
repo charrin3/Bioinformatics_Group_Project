cd3598bbdf6fc80dba3f149c1b04688dc8b91721
Leigh C Campbell <lcampbe3@remote201.helios.nd.edu>
2018-12-01 09:39:21 -0500

    all files

--- HMM_script.sh
+++ HMM_script.sh
@@ -8,7 +8,7 @@ do
 ~/muscle3.8.31_i86linux64 -in $i -out $(basename -s .fasta "$i").txt
 done
 
-#HMM 
+#HMM
 for j in *.txt
 do
 ~/local/bin/hmmbuild $(basename -s .txt "$j").hmm $j
@@ -18,12 +18,12 @@ for file_search in "Control1protein.fasta" "Control2protein.fasta" "Obese1protei
 do
 for file_build  in *.hmm
 do
-~/local/bin/hmmsearch --tblout $(basename -s .fasta "$file_search")_search.txt $file_build $file_search
+~/local/bin/hmmsearch --tblout $(basename -s .hmm "$file_build")_"$file_search"_cl.txt $file_build $file_search
 done
 done
 
-for file in *_search*
+for file in *_cl*
 do
-echo $(basename -s .txt "$file")
+echo $(basename -s _cl.txt "$file")
 grep -v '#' "$file" | wc -l
-done >> HMM_hits.txt
+done > HMM_hits.txt
