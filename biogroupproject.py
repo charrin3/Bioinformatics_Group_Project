aaa77a3a1473949fd423e26c5cd6ed1551088d93
Caitlin Harrington <cnharrington97@gmail.com>
2018-12-05 11:12:56 -0500

    updated graphing

--- biogroupproject.py
+++ biogroupproject.py
@@ -27,28 +27,27 @@ from plotnine import *
 
 #produce and save the graphs
 a=ggplot(hitsframe1,aes(x='hitsframe1.iloc[:,0]',y='hitsframe1.iloc[:,1]'))+xlab("sequence1")+ylab("hits")
-g=a+geom_bar(stat="summary")+scale_x_discrete(labels=l)
+g=a+geom_bar(stat="summary")+scale_x_discrete(labels=l)+ggtitle("Gsta2")
 ggsave(filename="sequence1hits.pdf", plot=g)
 
-
 b=ggplot(hitsframe2,aes(x='hitsframe2.iloc[:,0]',y='hitsframe2.iloc[:,1]'))+xlab("sequence2")+ylab("hits")
-g=b+geom_bar(stat="summary")+scale_x_discrete(labels=l)
+g=b+geom_bar(stat="summary")+scale_x_discrete(labels=l)+ggtitle("Slc7a12")
 ggsave(filename="sequence2hits.pdf", plot=g)
 
 c=ggplot(hitsframe6,aes(x='hitsframe6.iloc[:,0]',y='hitsframe6.iloc[:,1]'))+xlab("sequence6")+ylab("hits")
-g=c+geom_bar(stat="summary")+scale_x_discrete(labels=l)
+g=c+geom_bar(stat="summary")+scale_x_discrete(labels=l)+ggtitle("Ptpn5")
 ggsave(filename="sequence6hits.pdf", plot=g)
 
 d=ggplot(hitsframe8,aes(x='hitsframe8.iloc[:,0]',y='hitsframe8.iloc[:,1]'))+xlab("sequence8")+ylab("hits")
-g=d+geom_bar(stat="summary")+scale_x_discrete(labels=l)
+g=d+geom_bar(stat="summary")+scale_x_discrete(labels=l)+ggtitle("Atp12a")
 ggsave(filename="sequence8hits.pdf", plot=g)
 
 e=ggplot(hitsframe9,aes(x='hitsframe9.iloc[:,0]',y='hitsframe9.iloc[:,1]'))+xlab("sequence9")+ylab("hits")
-g=e+geom_bar(stat="summary")+scale_x_discrete(labels=l)
+g=e+geom_bar(stat="summary")+scale_x_discrete(labels=l)+ggtitle("Lhx2")
 ggsave(filename="sequence9hits.pdf", plot=g)
 
 f=ggplot(hitsframe10,aes(x='hitsframe10.iloc[:,0]',y='hitsframe10.iloc[:,1]'))+xlab("sequence10")+ylab("hits")
-g=f+geom_bar(stat="summary")+scale_x_discrete(labels=l)
+g=f+geom_bar(stat="summary")+scale_x_discrete(labels=l)+ggtitle("Synpr")
 ggsave(filename="sequence10hits.pdf", plot=g)
 
 
